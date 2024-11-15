84fa5b79b8ea05b31ab63a0226174c1becf1b03d
bgdnyrmln <bgdnyrmln@gmail.com>
2024-11-15 11:11:30 +0200

    qwe
    qwe

---
+++ script.js
@@ -0,0 +1,32 @@
+function appendnumber(number) {
+    document.getElementById("screen").innerHTML += number;
+}
+
+function clearscreen() {
+    document.getElementById("screen").innerHTML = "";
+}
+
+function calculate() {
+    var screen = document.getElementById("screen").innerHTML;
+    var history = screen;
+    var result = eval(screen);
+    history += "=" + result;
+    document.getElementById("screen").innerHTML = result;
+    document.getElementById("history").innerHTML = history;
+}
+
+function add() {
+    document.getElementById("screen").innerHTML += "+";
+}
+
+function subtract() {
+    document.getElementById("screen").innerHTML += "-";
+}
+
+function multiply() {
+    document.getElementById("screen").innerHTML += "*";
+}
+
+function divide() {
+    document.getElementById("screen").innerHTML += "/";
+}
