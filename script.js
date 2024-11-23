ccc980fc94df83d55ac7aeefb8f5a95f08e25947
bgdnyrmln <bgdnyrmln@gmail.com>
2024-11-23 19:40:39 +0200

    ыы
    ыы

--- script.js
+++ script.js
@@ -6,27 +6,58 @@ function clearscreen() {
     document.getElementById("screen").innerHTML = "";
 }
 
+
 function calculate() {
     var screen = document.getElementById("screen").innerHTML;
-    var history = screen;
-    var result = eval(screen);
-    history += "=" + result;
+    if (screen === "") return; 
+    var result = eval(screen); 
+
+    var newHistoryItem = document.createElement("div");
+    newHistoryItem.className = "history"; 
+    newHistoryItem.innerHTML = screen + " = " + result; 
+
+    var newHistoryButton = document.createElement("div");
+    newHistoryButton.innerHTML = "AC";
+    newHistoryButton.className = "button";
+    
+    newHistoryButton.addEventListener("click", function() {
+        newHistoryItem.remove(); 
+    });
+
+    newHistoryItem.appendChild(newHistoryButton);
+    var historyContainer = document.querySelector(".history-container");
+    historyContainer.appendChild(newHistoryItem);
+
     document.getElementById("screen").innerHTML = result;
-    document.getElementById("history").innerHTML += '\n' + history;
 }
 
+
 function add() {
-    document.getElementById("screen").innerHTML += "+";
+    appendOperator("+");
 }
 
 function subtract() {
-    document.getElementById("screen").innerHTML += "-";
+    appendOperator("-");
 }
 
 function multiply() {
-    document.getElementById("screen").innerHTML += "*";
+    appendOperator("*");
 }
 
 function divide() {
-    document.getElementById("screen").innerHTML += "/";
+    appendOperator("/");
+}
+
+// Общая функция для добавления оператора
+function appendOperator(operator) {
+    var screen = document.getElementById("screen").innerHTML;
+    if (screen === "" || isLastCharOperator(screen)) {
+        return; // Прерываем, если экран пуст или последний символ оператор
+    }
+    document.getElementById("screen").innerHTML += operator;
+}
+
+// Проверка: является ли последний символ оператором
+function isLastCharOperator(screen) {
+    return ["+", "-", "*", "/"].includes(screen[screen.length - 1]);
 }
